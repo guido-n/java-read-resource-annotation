1544c16aeb642da8ca9e06b78e08f222f3edb424
Guido Negro <guidon83@libero.it>
2017-01-10 23:09:04 +0000

    Implemented annotation interface

---
+++ src/main/java/com/guido/annotation/LoadResourceAsString.java
@@ -0,0 +1,19 @@
+package com.guido.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * LoadResourceAsString
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface LoadResourceAsString{
+
+    String absoluteResourcePath();
+
+}
