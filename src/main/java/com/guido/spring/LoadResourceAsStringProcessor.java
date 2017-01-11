13c670bb9559dc471e6141e173623f75fabb3241
Guido Negro <guidon83@libero.it>
2017-01-11 00:19:38 +0000

    Implemented processor and tests

--- src/main/java/com/guido/spring/LoadResourceAsStringProcessor.java
+++ src/main/java/com/guido/spring/LoadResourceAsStringProcessor.java
@@ -7,28 +7,81 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
 import org.springframework.util.ReflectionUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * LoadResourceAsStringProcessor
  */
+@Component
 public class LoadResourceAsStringProcessor implements BeanPostProcessor {
 
     private static final Logger log = LoggerFactory.getLogger(LoadResourceAsStringProcessor.class);
 
+    /**
+     * Load a resource as a String
+     *
+     * @param resourcePath
+     * @param clazz class used to access the resource
+     *
+     * @return
+     */
+    private static String loadResourceAsString(String resourcePath, Class<?> clazz) {
+        String resourceContent = "";
+        try {
+            resourceContent = new String(Files.readAllBytes(Paths.get(clazz.getResource(resourcePath).toURI())), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            log.error("loadStringFromResource(): couldn't load resource \"{}\"", resourcePath);
+
+            // we could throw an unchecked exception here and make the loading to break when a resource is not found
+            // throw new RuntimeException(String.format("Couldn't load resource: %s", resourcePath), e);
+
+        }
+        return resourceContent;
+    }
+
+    /**
+     *
+     * @param bean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         return bean;
     }
 
+    /**
+     *
+     * @param bean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
 
         ReflectionUtils.doWithFields(
                 bean.getClass(),
                 (field) -> {
-                    if (field.getAnnotation(LoadResourceAsString.class) != null) {
-                        log.info("postProcessAfterInitialization(): {}", field.getName());
+                    LoadResourceAsString annotation = field.getAnnotation(LoadResourceAsString.class);
+                    if (annotation != null) {
+
+                        log.info(
+                                "postProcessAfterInitialization(): ({}) {}: \"{}\"",
+                                field.getClass().getCanonicalName(),
+                                field.getName(),
+                                annotation.value()
+                        );
+
+                        field.setAccessible(true);
+                        field.set( bean, loadResourceAsString(annotation.value(), field.getClass()) );
+                        field.setAccessible(false);
                     }
                 }
         );
--- src/test/java/com/guido/AnnotationTest.java
+++ src/test/java/com/guido/AnnotationTest.java
@@ -1,13 +1,18 @@
 package com.guido;
 
 import com.guido.annotation.LoadResourceAsString;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 /**
  * AnnotationTest
@@ -19,15 +24,20 @@ public class AnnotationTest {
     private static final Logger log = LoggerFactory.getLogger(AnnotationTest.class);
 
     @LoadResourceAsString("/text/resource.txt")
-    private static String queryStatic;
+    private static String staticText;
 
     @LoadResourceAsString("/text/resource.txt")
-    private String query;
+    private String text;
+
+    @LoadResourceAsString("/text/non-existent.txt")
+    private String badText;
 
     @Test
     public void annotationTest() {
-        log.info("annotationTest(): queryStatic: \"{}\"", queryStatic);
-        log.info("annotationTest(): query: \"{}\"", query);
-    }
 
+        assertThat(staticText, equalTo("RESOURCE CONTENT"));
+        assertThat(text, equalTo("RESOURCE CONTENT"));
+        assertThat(badText, equalTo(""));
+
+    }
 }
